e4b5d9ab7bc88384f6c660f4820d4979a7b5896a
Stanimir Stoev <stoevs@gmail.com>
2016-08-30 04:27:10 +0300

    Minor fixes on folder names

--- 02. Code_Formatting/Homework/CodeFormattingHomework/Events/Properties/AssemblyInfo.cs
+++ 02. Code_Formatting/Homework/CodeFormattingHomework/Events/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeFormattingHomework")]
+[assembly: AssemblyTitle("Events")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeFormattingHomework")]
+[assembly: AssemblyProduct("Events")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
